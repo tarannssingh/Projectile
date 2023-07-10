35e8b6b97640e178d1e16ffbef172e6666d4bdfd
Rohan Udupa <86579136+lab596@users.noreply.github.com>
2023-07-10 09:53:15 -0700

    Adding ivelocity and angle calculation

--- Panel.java
+++ Panel.java
@@ -12,6 +12,7 @@ import java.lang.Math;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.Math;
 
 //makes sure to add Action Listener to eventually allow us to compute user action on the screen
 public class Panel extends JPanel implements MouseListener, ActionListener{
@@ -44,6 +45,14 @@ public class Panel extends JPanel implements MouseListener, ActionListener{
     static int mousexpos;
     static int mouseypos;
 
+    //The intersection point of the mouse release and projectile position
+    static int intersectionx;
+    static int intersectiony;
+    //The length of the opposite and adjacent sides of the triangle formed by the intersection point and the projectile
+    static double opposite;
+    static double adjacent;
+    static double hypotenuse;
+
     //Variables used to allow for ticks to function (defined later)
     int DELAY = 5;
     Timer timer;
@@ -61,8 +70,8 @@ public class Panel extends JPanel implements MouseListener, ActionListener{
         //setting our frame to the width and height of the users device 
         frame.setSize((int) screenSize.getWidth(),(int) screenSize.getHeight());
         //This sets the initila position of the projectile to be at the bottom of the screen
-        projx = 200;
-        projy = ScreenHeight - 150;
+        projx = 500;
+        projy = ScreenHeight - 300;
         //This implements the mouse listener to the panel allowing for the @Overide methods to be used (bottom)
         frame.addMouseListener(new Panel());
         
@@ -99,28 +108,50 @@ public class Panel extends JPanel implements MouseListener, ActionListener{
         //ground
         g.setColor(new Color(0,100,0));
         g.fillRect(0,ScreenHeight-90,ScreenWidth,90); 
-        //projectile (if shot)
+        
         if(shoot == true){
+            //projectile (if shot)
             g.setColor(Color.red);
             g.fillOval(projx,projy,PIXEL_SIZE,PIXEL_SIZE);
+            //seeing the intersection triangle
+            g.fillOval(mousexpos,mouseypos,10,10);
+            ///g.drawLine(mousexpos+5,mouseypos,mousexpos+5,0);
+            ///g.drawLine(projx+5,projy+9,0,projy+9);
+            g.fillOval(mousexpos,projy+4,10,10);
         }
     }
 
     //The following methods are used to determine if a mouse action has been performed
     @Override
     public void mousePressed(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        System.out.println("Mouse Clicked");
-        
-        
+        System.out.println("Mouse Clicked");  
     }
 
     @Override
     public void mouseReleased(java.awt.event.MouseEvent e) {
         //Getting the location of the mouse when it is released
         PointerInfo info = MouseInfo.getPointerInfo();
-        System.out.println(info.getLocation());
-        shoot = true;  
+        mousexpos = (int) info.getLocation().getX()-12;
+        mouseypos = (int) info.getLocation().getY()-35;
+        //Finding the intersection point of the two points
+        intersectionx= mousexpos;
+        intersectiony= projy+4;
+        //Finding the length of the two sides of the triangle formed by the intersection point and the projectile
+        adjacent = projx - intersectionx;
+        opposite = mouseypos - intersectiony;
+        //Using trig to get the angle of the shot
+        angle = Math.toDegrees(Math.atan(opposite/adjacent));
+        System.out.println("Angle: " + angle);
+        //Using the hypotenuse to act as the initial velocity of the shot
+        hypotenuse = Math.sqrt(Math.pow(adjacent,2)+Math.pow(opposite,2));
+        ivelocity = hypotenuse/1.5;
+        //checking to see if the shot is behind and lower than the ball
+        if(mousexpos<500 && mouseypos>ScreenHeight-300){
+            shoot = true;
+        }
+        else{
+            System.out.println("Invalid Shot");
+        } 
     }
 
     @Override
